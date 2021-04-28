849f2e1ef6115603ac010b5e6d823d4bcbc1faaa
Jaden <jaden.helms@cvtechonline.net>
2021-04-28 13:12:55 -0500

    added js

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var receipt = [];
+
+
+
+
+function addToReceipt() {
+    console.log("Test");
+}
+
+function submitReceipt() {
+    receipt.forEach(getPrices);
+}
+
+function getPrices(item) {
+    fName = { "fName":item};
+    dbParam = JSON.stringify(fName);
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            items_price = JSON.parse(this.responseText);
+            for (x in items_price) {
+                txt += items_price[x].name + "<br>";
+            }
+            receiptDisplay.innerHTML = txt;
+        }
+    };
+xmlhttp.open("GET", "menu_items.php?x=" + dbParam, true);
+xmlhttp.send();
+}
